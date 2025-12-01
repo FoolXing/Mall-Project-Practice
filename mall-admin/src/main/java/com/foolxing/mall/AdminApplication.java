1230038fea74b688c592c47cf5011f1f2ab882ea
Foolxing <foolxing@163.com>
2025-12-01 18:37:19 +0800

    创建项目

---
+++ mall-admin/src/main/java/com/foolxing/mall/AdminApplication.java
@@ -0,0 +1,15 @@
+package com.foolxing.mall;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@MapperScan("com.foolxing.mall.mapper")
+@EnableCaching
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
