c35effb838fcec58fbfad4ff86a780bc8837b4d2
Foolxing <foolxing@163.com>
2025-12-01 19:04:11 +0800

    创建需要的项目

--- mall-common/src/main/java/com/foolxing/mall/Test.java
+++ mall-common/src/main/java/com/foolxing/mall/Test.java
@@ -3,5 +3,6 @@ package com.foolxing.mall;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Test.main");
+        System.out.println("Test.main");
     }
 }
