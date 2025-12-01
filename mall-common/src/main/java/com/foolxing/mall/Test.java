9a09d477fab01ece70b98ba1895f7888dbc33ae8
Foolxing <foolxing@163.com>
2025-12-01 19:38:27 +0800

    修了一个小问题

--- mall-common/src/main/java/com/foolxing/mall/Test.java
+++ mall-common/src/main/java/com/foolxing/mall/Test.java
@@ -3,6 +3,5 @@ package com.foolxing.mall;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Test.main");
-        System.out.println("Test.main");
     }
 }
