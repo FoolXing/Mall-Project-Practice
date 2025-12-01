552a81b7aff4ead8b4001888039ca3d78b2cdf4d
Foolxing <foolxing@163.com>
2025-12-01 20:00:35 +0800

    实现一个简单的list请求

--- mall-admin/src/main/java/com/foolxing/mall/controller/AdminController.java
+++ mall-admin/src/main/java/com/foolxing/mall/controller/AdminController.java
@@ -1,9 +1,15 @@
 package com.foolxing.mall.controller;
 
 
+import com.foolxing.mall.pojo.Admin;
+import com.foolxing.mall.service.IAdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,6 +22,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
-
+    @Autowired
+    private IAdminService adminService;
+    @GetMapping("/list")
+    public List<Admin> list (){
+        return adminService.list();
+    }
 }
 
